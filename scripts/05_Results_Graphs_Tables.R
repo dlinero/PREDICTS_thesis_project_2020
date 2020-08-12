395763c7e1dc1f8eae324ef001d098f330f30c8b
dlinero <dl1119@ic.ac.uk>
2020-08-13 00:01:02 +0100

    Orders graph

--- scripts/05_Results_Graphs_Tables.R
+++ scripts/05_Results_Graphs_Tables.R
@@ -1,5 +1,7 @@
 # Load libraries
 library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
 
 
 # Create a vector with the total number of sources, influential sources
@@ -41,4 +43,75 @@ ggplot(number_influential, aes(number_sources, ID, color = statistic)) +
   theme_bw() 
   
 # Clear plot
+dev.off()
+
+# Orders -------------------------------------------------------------------------
+
+# Import table
+frugi_endooPlants_notEndooPlants <- readRDS(file = "./output/cleaned_data/01_Filter_data_frugi_endooPlants_notEndooPlants_records.rds")
+
+# Calculate number of records per order for frugivores
+frugi <- frugi_endooPlants_notEndooPlants %>% subset(Kingdom == "Animalia") %>%
+  droplevels() 
+table(frugi$Order)
+
+# Calculate number of records per order for endozoochorous plants
+endo <- frugi_endooPlants_notEndooPlants %>% subset(Kingdom == "Plantae") %>%
+  droplevels() 
+table(endo$Order)
+
+# Calculate number of records per order for abiotically-dispersed plants
+abi <- frugi_endooPlants_notEndooPlants %>% subset(Kingdom == "nePlantae") %>%
+  droplevels() 
+table(abi$Order)
+
+# Calculate number of species per order for frugivores
+frugi <- frugi %>% distinct(Best_guess_binomial, .keep_all = TRUE)
+table(frugi$Order)
+
+# Calculate number of species per order for endo
+endo <- endo %>% distinct(Best_guess_binomial, .keep_all = TRUE)
+table(endo$Order)
+
+# Calculate number of species per order for abi
+abi <- abi %>% distinct(Best_guess_binomial, .keep_all = TRUE)
+table(abi$Order)
+
+# Import summary table 
+orders <- read.csv("./output/graphs_tables/orders.csv", sep= ";")
+
+# Ordering the functional groups
+orders$group <- factor(orders$group, 
+                       levels = c("Aves", "Mammalia", 
+                                  "Endozoochorous plants", 
+                                  "Abiotically-dispersed plants"))
+# Eliminate spaces between words
+orders$order <-trimws(orders$order)
+
+# Export graph
+pdf(file = "./output/graphs_tables/05_Results_Graphs_Tables_Orders.pdf", width = 14)
+
+
+ggplot(orders) + 
+  # barplot
+  geom_bar(
+    # Organize the bars in descending order and give different colors to the orders
+    aes(x = reorder(order, -species), y = species, fill = order, group = order), 
+    stat='identity', position = 'dodge'
+  ) +
+  # Add a number with the number of records for every order
+  geom_text(
+    aes(x = order, y = species, label = records, group = order),
+    position = position_dodge(width = 1),
+    vjust = -0.3, size = 3, fontface = "bold"
+  ) + 
+  # Remove legend and give axis labels
+  theme_bw() + theme(legend.position = "none") + labs(y = "Number of species",
+                                                      x = "Order") +
+  # Faceted plot of groups, allow the order names to be different in every facet
+  facet_wrap( ~ group, scales = "free_y", strip.position = 'top') +
+  # Rotate graph
+  theme(strip.placement = "outside", axis.text=element_text(size=7)) + coord_flip()
+
+# Clear
 dev.off()
